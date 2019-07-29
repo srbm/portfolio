f408cc7907eacead295da0e9c8f6015694db6152
srbm <shawnrbm@gmail.com>
2019-07-29 13:34:48 -0700

    working open/close of 1  modal

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 'use strict';
 function openModal() {
+    const modalHTML = `
+    <div class="examples__modal">
+    <h2>LongHorn Steakhouse<span>X</span></span></h2>
+    <figure>
+        <img src="images/lhsh-ss1.png" alt="screenshot of longhorn steakhouse header section">
+    </figure>
+    <h5>HTML, CSS, JavaScript, Green Sock, Scroll Magic</h5>
+    <a class="link-button" href="http://www.longhornsteakhouse.com/app">See it live</a>
+    <figure>
+        <img src="images/lhsh-ss2.png" alt="screenshot of longhorn steakhouse header section">
+    </figure>
+    <p>For this project I completed the development in under 2 days using HTML/CSS and JavaScript with the help of the Green Sock and Scroll Magic libraries.</p>
+    </div>`;
     let width = parseInt(window.innerWidth)
     $('.examples__card').on('click', e => {
         const target = e.currentTarget;
         console.log($(target).next());
         if (width < 996) {
-            $(target).next().append(`
-            <h2>LongHorn Steakhouse<span>X</span></span></h2>
-            <figure>
-                <img src="images/lhsh-ss1.png" alt="screenshot of longhorn steakhouse header section">
-            </figure>
-            <a class="link-button" href="http://www.longhornsteakhouse.com/app">See it live</a>
-            <h5>HTML, CSS, JavaScript, Green Sock, Scroll Magic</h5>
-            <figure>
-                <img src="images/lhsh-ss2.png" alt="screenshot of longhorn steakhouse header section">
-            </figure>
-            <p>For this project I completed the development in under 2 days using HTML/CSS and JavaScript with the help of the Green Sock and Scroll Magic libraries.</p>
-            `);
+            $(target).next().append(modalHTML);
         } else {
-            $('.big-screen-modal1').append(`
-            <h2>LongHorn Steakhouse<span>X</span></span></h2>
-            <figure>
-                <img src="images/lhsh-ss1.png" alt="screenshot of longhorn steakhouse header section">
-            </figure>
-            <a class="link-button" href="http://www.longhornsteakhouse.com/app">See it live</a>
-            <h5>HTML, CSS, JavaScript, Green Sock, Scroll Magic</h5>
-            <figure>
-                <img src="images/lhsh-ss2.png" alt="screenshot of longhorn steakhouse header section">
-            </figure>
-            <p>For this project I completed the development in under 2 days using HTML/CSS and JavaScript with the help of the Green Sock and Scroll Magic libraries.</p>
-            `);
-            $('.big-screen-modal1').slideToggle('200ms');
+            if ( $('#big-screen-modal1').html() ) {
+                $('#big-screen-modal1').slideUp('50ms').empty().append(modalHTML).slideDown('200ms');
+            } else {
+                $('#big-screen-modal1').append(modalHTML).slideDown('200ms');
+            }
         }
-        
     });
 }
 function closeModal() {
-
-    $('#modal-longhorn').on('click', () => {
-        $('#modal-longhorn').slideUp('100ms');
+    $('#big-screen-modal1').on('click', () => {
+        $('#big-screen-modal1').slideUp('100ms').empty().animate({
+            scrollTop: $('#examples').offset().top
+        }, 2000);
     });
 }
 function watchFunctions() {
