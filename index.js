a91ff47879270ec67d7160a14c49334cef027597
srbm <shawnrbm@gmail.com>
2019-07-29 16:22:29 -0700

    all projects js configured

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
 'use strict';
 function openModal() {
-    const modalHTML = `
-    <div class="examples__modal">
-    <h2>LongHorn Steakhouse<span>X</span></span></h2>
-    <figure>
-        <img src="images/lhsh-ss1.png" alt="screenshot of longhorn steakhouse header section">
-    </figure>
-    <h5>HTML, CSS, JavaScript, Green Sock, Scroll Magic</h5>
-    <a class="link-button" href="http://www.longhornsteakhouse.com/app">See it live</a>
-    <figure>
-        <img src="images/lhsh-ss2.png" alt="screenshot of longhorn steakhouse header section">
-    </figure>
-    <p>For this project I completed the development in under 2 days using HTML/CSS and JavaScript with the help of the Green Sock and Scroll Magic libraries.</p>
-    </div>`;
+    const modalHTML = [
+        `<div class="examples__modal">
+        <h2>LongHorn Steakhouse<span>X</span></span></h2>
+        <figure>
+            <img src="images/lhsh-ss1.png" alt="screenshot of longhorn steakhouse header section">
+        </figure>
+        <h5>HTML, CSS, JavaScript, Green Sock, Scroll Magic</h5>
+        <a class="link-button" href="http://www.longhornsteakhouse.com/app">See it live</a>
+        <figure>
+            <img src="images/lhsh-ss2.png" alt="screenshot of longhorn steakhouse header section">
+        </figure>
+        <p>For this project I completed the development in under 2 days using HTML/CSS and JavaScript with the help of the Green Sock and Scroll Magic libraries.</p>
+        </div>`,
+        `<div class="examples__modal">
+        <h2>Cloud Cap Ventures<span>X</span></span></h2>
+        <figure>
+            <img src="images/ccv-ss1.png" alt="screenshot of cloud cap ventures body content section">
+        </figure>
+        <h5>WordPress, Sass, Events</h5>
+        <a class="link-button" href="http://cloudcapventures.com">See it live</a>
+        <figure>
+            <img src="images/ccv-ss2.png" alt="screenshot of cloud cap ventures body content section">
+        </figure>
+        <p>I built this site from scratch on my own using WordPress and a few plugins to help make a fully functional page. Users can sign up and reserve a spot for a trip and pay using a paypal gateway.</p>
+        </div>`,
+        `<div class="examples__modal">
+        <h2>Portland Adventures Weather<span>X</span></span></h2>
+        <figure>
+            <img src="images/paw-ss.png" alt="screenshot of portland adventure weather full page app.">
+        </figure>
+        <h5>React, JavaScript, APIs, Sass</h5>
+        <a class="link-button" href="http://srbm.github.io/pdx-adventure-weather">See it live</a>
+        <p>I wanted to create a place for Portlanders to see what the weather is like in places outisde of town where they might going adventuring. This is a single page app the gives current temperature and a radar map of the area you click on. I used React and API functionality to pull this together.</p>
+        </div>`,
+    ];
     let width = parseInt(window.innerWidth)
     $('.examples__card').on('click', e => {
         const target = e.currentTarget;
-        console.log($(target).next());
+        let i = modalPicker(target, modalHTML);
         if (width < 996) {
-            $(target).next().append(modalHTML);
+            if ( $(target).next().html() ) {
+                $(target).next().slideUp('50ms').empty();
+                $(target).next().append(modalHTML[0]).slideDown('200ms');
+            } else {
+                $(target).next().append(modalHTML[0]).slideDown('200ms');
+            }
         } else {
             if ( $('#big-screen-modal1').html() ) {
-                $('#big-screen-modal1').slideUp('50ms').empty().append(modalHTML).slideDown('200ms');
+                $('#big-screen-modal1').slideUp('50ms').empty();
+                $('#big-screen-modal1').append(modalHTML[i]).slideDown('200ms');
             } else {
-                $('#big-screen-modal1').append(modalHTML).slideDown('200ms');
+                $('#big-screen-modal1').append(modalHTML[i]).slideDown('200ms');
             }
         }
     });
 }
+function modalPicker(target, arr) {
+    console.log('picking');
+    if ( $(target).next().is('#longhorn-modal')) {
+        console.log('longhorn');
+        return 0;
+    } else if ( $(target).next().is('#cloud-cap-modal') ) {
+        console.log('cloud cap');
+        return 1;
+    } else {
+        console.log('weather');
+        return 2;
+    }
+}
 function closeModal() {
-    $('#big-screen-modal1').on('click', () => {
-        $('#big-screen-modal1').slideUp('100ms').empty().animate({
+    $('.modal-cont').on('click', e => {
+        console.log('close click');
+        $(e.currentTarget).slideUp('100ms').empty().animate({
             scrollTop: $('#examples').offset().top
-        }, 2000);
+        }, 1000);
     });
 }
 function watchFunctions() {
