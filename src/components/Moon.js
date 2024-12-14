e6e21aef484c3812b71b026f4452cdfb0bd652dd
srbm <shawn.meyer@whoisowenjones.com>
2024-12-13 16:14:52 -0800

    moon api implemented, css phase visibility and moon component

--- src/components/Moon.js
+++ src/components/Moon.js
@@ -1,57 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import '../styles/components/Moon.css';
 
-const MOON_ENDPOINT = 'https://api.astronomyapi.com/api/v2/studio/moon/phases';
-const API_KEY = process.env.REACT_APP_APPLICATION_ID;
-const API_SECRET = process.env.REACT_APP_APPLICATION_SECRET;
-
+const MOON_ENDPOINT = 'https://api.stormglass.io/v2/astronomy/point';
+const API_KEY = process.env.REACT_APP_STORM_GLASS;
 const headers = {
     'Content-Type': 'application/json',
     "X-Requested-With": "XMLHttpRequest",
-    Authorization: `Basic ${btoa(`${API_KEY}:${API_SECRET}`)}`
+    'Authorization': API_KEY
 };
-const params = (date) => ({
-  observer: {
-    date: date.toISOString().split('T')[0], // Using ISOString and splitting to get 'YYYY-MM-DD'
-  }
-});
 
-// Changed to return the fetched data
+const date = new Date();
+const end = date.toISOString().split('T')[0]; // 'YYYY-MM-DD'
+// My approximate location
+const lat = 45.3;
+const lng = 122.3;
+
 const fetchMoonPhase = async () => {
   try {
-    const response = await axios.post(MOON_ENDPOINT, params(new Date()), { headers });
-    console.log(response.data);
-    return response.data;
+    const response = await fetch(`https://api.stormglass.io/v2/astronomy/point?lat=${lat}&lng=${lng}&end=${end}`, {
+      headers
+    });
+    return response;
+
   } catch (error) {
     console.error('Error fetching moon phase:', error);
     return null;
   }
+
 };
 
+
 const MoonPhase = () => {
+
   const [moonPhase, setMoonPhase] = useState(null);
 
   useEffect(() => {
-    const updateMoonPhase = async () => {
-      const currentMoonPhase = await fetchMoonPhase();
-      setMoonPhase(currentMoonPhase);
-    };
+      async function fetchAndSetMoonPhase() {
+          try {
+              const response = await fetchMoonPhase();
+              if (response.ok) {
+                const phaseData = await response.json();
+                const phase = phaseData.data[0].moonPhase.current.value * 360;
+                setMoonPhase(phase);
+              } else {
+                throw new Error('Failed to fetch moon phase: ' + response.status);
+              }
+          } catch (error) {
+              console.error('Error fetching the moon phase. Here\'s a random moon', error);
+              const phase = Math.random() * 360;
+              setMoonPhase(phase);
+          }
+      }
 
-    updateMoonPhase();
-
-    const intervalId = setInterval(updateMoonPhase, 3600000); // Update every hour
-
-    return () => clearInterval(intervalId);
+      fetchAndSetMoonPhase();
   }, []);
+
+  const dividerRotation = {
+    transform: `rotate3d(0, 1, 0, ${moonPhase}deg)`
+  }
+  const leftHemiClass = moonPhase < 180 ? "dark" : "light";
+  const rightHemiClass = moonPhase < 180 ? "light" : "dark";
+  
 
   // Display the moon phase image
   return (
-    <div className='moonphase'>
-      {/* Display the moon phase using a state variable */}
-      {moonPhase && (
-        <img src={moonPhase.imageUrl} alt="Current Moon Phase" />
-      )}
+    <div className="moon-container">
+      <div className="sphere">
+        <div className={`hemisphere ${leftHemiClass}`}></div>
+        <div className={`hemisphere ${rightHemiClass}`}></div>
+        <div className="divider" style={dividerRotation}></div>
+      </div>
+        {/* <img src='https://www.pennlive.com/resizer/dp_dvs4Zo8pljrfn2kUqoERJ90M=/800x0/smart/image.pennlive.com/home/penn-media/width600/img/wildaboutpa/photo/14-blue-moonjpg-e541a63d92f2ed42.jpg' alt='moon' /> */}
     </div>
   );
 };
